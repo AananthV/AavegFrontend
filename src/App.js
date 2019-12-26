2c7f06e56a0496a2a55227ec3d78a257d479b8fa
vikram710 <vikram710v@gmail.com>
2019-12-27 00:59:12 +0530

    [Feat] About page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router,Route} from "react-router-dom";
-import Team from './components/Teams';
+import Team from './components/Team';
 import Footer from './components/Footer'
 import Header from './components/Header'
+import About from './components/About'
 
 class App extends Component {
 	render() {
 		return (
 		<Router>
 		<Route path="/"   component= {Header}/>
-		<Route path="/teams"   component= {Team}/>
+		<Route path="/team"   component= {Team}/>
+		<Route path="/about"   component= {About}/>
 		<Route path="/"   component= {Footer}/>
 		</Router>
     	);
