303621bfb1c06ebf7456866b93b15f48760a6c3f
Aananth <aananthv@outlook.com>
2019-12-28 11:57:02 +0530

    [Fix] Remove props call after redirect

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -27,42 +27,41 @@ class LoginForm extends Component {
         const user = {
             rollnumber: this.state.rollno,
             password: this.state.password
-          };
-          axios.post(process.env.REACT_APP_API_BASE_URL+'api/studentLogin', qs.stringify(user),{
+        };
+        axios.post(process.env.REACT_APP_API_BASE_URL+'api/studentLogin', qs.stringify(user),{
             headers: {
                 'Content-type': 'application/x-www-form-urlencoded'
-            }})
-            .then(res => {
-                console.log(res.data)
-                sessionStorage.setItem('user_id', res.data.user_id);
-                sessionStorage.setItem('APIToken', res.data.APIToken);
-                this.setState({redirect:true})
-                this.props.close()
-            })
+            }
+        }).then(res => {
+            console.log(res.data)
+            sessionStorage.setItem('user_id', res.data.user_id);
+            sessionStorage.setItem('APIToken', res.data.APIToken);
+            this.setState({redirect:true})
+        })
     }
 
-	render(){
-        var redirect = this.state.redirect;
-        if(redirect){
-            return <Redirect to='/home'/>
-        }
-		return <div>
-            <Form onSubmit={this.handleSubmit.bind(this)}>
-                <Form.Group controlId="formGroupEmail">
-                    <Form.Label>Roll Number</Form.Label>
-                    <Form.Control type="number" placeholder="Roll Number" onChange={this.handleChange.bind(this)} />
-                </Form.Group>
-
-                <Form.Group controlId="formGroupPassword">
-                    <Form.Label>Webmail Password</Form.Label>
-                    <Form.Control type="password" placeholder="Webmail Password" onChange={this.handleChange.bind(this)} />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Login
-                </Button>
-            </Form>
-		</div>
-
-	}
+  	render(){
+          var redirect = this.state.redirect;
+          if(redirect){
+              return <Redirect to='/'/>
+          }
+      		return (
+            <div>
+                <Form onSubmit={this.handleSubmit.bind(this)}>
+                    <Form.Group controlId="formGroupEmail">
+                        <Form.Label>Roll Number</Form.Label>
+                        <Form.Control type="number" placeholder="Roll Number" onChange={this.handleChange.bind(this)} />
+                    </Form.Group>
+                    <Form.Group controlId="formGroupPassword">
+                        <Form.Label>Webmail Password</Form.Label>
+                        <Form.Control type="password" placeholder="Webmail Password" onChange={this.handleChange.bind(this)} />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Login
+                    </Button>
+                </Form>
+            </div>
+          )
+  	}
 }
 export default LoginForm;
