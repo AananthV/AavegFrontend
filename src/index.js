b23f67db2b400708fd773b3caf1313a47720b4fc
Aananth <aananthv@outlook.com>
2020-01-23 08:54:43 +0530

    Change BG Based on Hostel

--- src/index.js
+++ src/index.js
@@ -8,6 +8,16 @@ import * as serviceWorker from './serviceWorker'
 const colorCodes = ['#2a34c4', '#2e4800', '#e31a1a', '#e9e9e9', '#ffe399']
 //document.getElementsByTagName('body')[0].style.backgroundImage = `linear-gradient(315deg, #000000 0%, ${colorCodes[localStorage.getItem('key')]} 90%)`
 
+const backgroundImages = {
+  'agate': 'BG_purple.jpg',
+  'azurite': 'BG_green.jpg',
+  'bloodstone': 'BG.jpg',
+  'cobalt': 'BG_silver.jpg',
+  'opal': 'BG_gold.jpg'
+}
+
+document.getElementsByTagName('body')[0].style.backgroundImage = 'url(/images/' + backgroundImages[localStorage.getItem('hostel')] + ')';
+
 ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
